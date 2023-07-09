f3edf9fd95bbe440b06b870a8a16b15fe0c7bf90
Daniil Palagin <palagdan@cvut.cz>
2023-07-09 15:25:18 +0200

    Fixed \r symbol overlapping in a username

--- app/chat_session.cpp
+++ app/chat_session.cpp
@@ -3,29 +3,38 @@
 
 void ChatSession::start() {
     m_sessions.push_back(shared_from_this());
-    write("Welocome, provide a username to use this chat\n");
+    write("Welcome, provide a username for using this chat!\n");
     readUsername();
 }
 
 void ChatSession::readUsername() {
     boost::asio::async_read_until(m_client_socket, m_inputBuffer, '\n',
-                                  [this, self=shared_from_this()](boost::system::error_code ec, std::size_t size){
-                                      if(!ec){
+                                  [this, self = shared_from_this()](boost::system::error_code ec, std::size_t size) {
+                                      if (!ec) {
                                           m_username = readBufferToString();
-                                          broadcast(m_username + " joined to the chat!\n");
-                                          startReading();
-                                      }else{
+                                          if (m_username.empty()) {
+                                              // Handle empty username
+                                              closeSession();
+                                          } else {
+                                              std::string msg = m_username + " joined the chat!\n";
+                                              broadcast(msg);
+                                              // Start reading after the username is successfully read
+                                              startReading();
+                                          }
+                                      } else {
                                           closeSession();
                                       }
-                                  });
+                                  }
+    );
 }
+
 
 void ChatSession::startReading() {
     boost::asio::async_read_until(m_client_socket, m_inputBuffer, '\n',
                                   [this, self = shared_from_this()](boost::system::error_code ec, std::size_t size){
                                       if(!ec){
                                           std::string msg = readBufferToString();
-                                          broadcast(m_username + ": " + msg);
+                                          broadcast(m_username + ": " + msg + "\n");
                                           startReading();
                                       }else{
                                           closeSession();
@@ -44,10 +53,12 @@ std::string ChatSession::readBufferToString(){
     std::istream input(&m_inputBuffer);
     std::string msg;
     std::getline(input, msg);
+    if (!msg.empty() && msg.back() == '\r')
+        msg.pop_back(); // Remove carriage return ('\r') if present
     return msg;
 }
 
-void ChatSession::write(const std::string &msg) {
+void ChatSession::write(const std::string & msg) {
     boost::asio::async_write(m_client_socket, boost::asio::buffer(msg),
                              [this, self=shared_from_this()](boost::system::error_code ec, std::size_t size){
                                  if(ec){
